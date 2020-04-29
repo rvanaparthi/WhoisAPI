f99defef734b91fc0649db7a1b39f303ee142fc0
Rajesh Vanaparti <vanaparthi.rajesh@gmail.com>
2020-04-29 15:30:11 -0400

    Adding the Powershell Script

--- run.ps1
+++ run.ps1
@@ -1,17 +1,3 @@
-# Input bindings are passed in via param block.
-param($InputBlob, $TriggerMetadata, $Timer) 
-
-
-# Write out the blob name and size to the information log.
-Write-Host "PowerShell Blob trigger function Processed blob! Name: $($TriggerMetadata.Name) Size: $($InputBlob.Length) bytes"
-
-# The 'IsPastDue' porperty is 'true' when the current function invocation is later than scheduled.
-if ($Timer.IsPastDue) {
-    Write-Host "PowerShell timer is running late!"
-}
-
-
-
 # Replace with your Primary Key
 $sharedKey = "$env:workspacekey"
 
